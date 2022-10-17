0162aa120c0d9b97a4321553c6d582be2e3adb97
zd2L20 <zd2L20@gmai.com>
2022-10-18 01:34:15 +0900

    ゴミ出しスケジュールのhtml一区切り

--- src/main/java/com/example/app/controller/HomeController.java
+++ src/main/java/com/example/app/controller/HomeController.java
@@ -1,24 +1,22 @@
 package com.example.app.controller;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.example.app.domain.Schedule;
 import com.example.app.domain.User;
-import com.example.app.service.UserService;
 
 
 @Controller
 public class HomeController {
 	
-	@Autowired
-	private UserService service;
+	//@Autowired
+	//private UserService service;
 	
 	@GetMapping({"/", "/home"})
 	public String home(@AuthenticationPrincipal  User user) {
@@ -30,9 +28,17 @@ public class HomeController {
 	@GetMapping({"/user", "/user/home"})
 	public String userPage(@AuthenticationPrincipal  User user, Model model) {
 		//予定表示
-		List<Schedule> scheduleList = service.getAll(user.getId());
-		model.addAttribute("schedule", scheduleList);
+		//List<Schedule> scheduleList = service.getAll(user.getId());
+		//model.addAttribute("schedule", scheduleList);
 		model.addAttribute("today",LocalDate.now());
+		
+		List<String> g = new ArrayList<>();
+		g.add("ビン");
+		g.add("カン");
+		g.add("プラスチック");
+		System.out.println(g.size());
+		model.addAttribute("garbageList", g);
+		
 		return "userPage";
 	}
 	
