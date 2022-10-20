deb47bae78b56e458d3ed771588ecedf691862e1
zd2L20 <zd2L20@gmail.com>
2022-10-20 10:18:52 +0900

    今日のゴミを表示した

--- src/main/java/com/example/app/controller/HomeController.java
+++ src/main/java/com/example/app/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.example.app.controller;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.format.TextStyle;
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -17,7 +19,6 @@ import com.example.app.service.UserService;
 
 @Controller
 public class HomeController {
-	
 	@Autowired
 	private UserService service;
 	
@@ -33,42 +34,49 @@ public class HomeController {
 		//予定表示
 		List<Schedule> schedules = service.getSchedule(user.getId());
 		StringBuilder[] strb = new StringBuilder[7];
-		strb[0] = new StringBuilder();
-		strb[1] = new StringBuilder();
-		strb[2] = new StringBuilder();
-		strb[3] = new StringBuilder();
-		strb[4] = new StringBuilder();
-		strb[5] = new StringBuilder();
-		strb[6] = new StringBuilder();
+		Locale locale = Locale.getDefault();
+		for(int i = 1; i <= 7; i++) {
+			strb[i-1] = new StringBuilder(DayOfWeek.of(i).getDisplayName(TextStyle.SHORT,locale));
+			//strb[i-1] = new StringBuilder(DayOfWeek.of(i).getDisplayName(TextStyle.SHORT,Locale.US));
+			strb[i-1].append(":");
+		}
 		for (Schedule s : schedules) {
 			System.out.println(s);
 			var dow = s.getDayOfWeek();
 			String str = s.getGarbage();
 			switch (dow) {
 			case 1:
-				strb[0].append(str + " ");
+				strb[0].append(str + ",");
 				break;
 			case 2:
-				strb[1].append(str+ " ");
+				strb[1].append(str+ ",");
 				break;
 			case 3:
-				strb[2].append(str+ " ");
+				strb[2].append(str+ ",");
 				break;
 			case 4:
-				strb[3].append(str+ " ");
+				strb[3].append(str+ ",");
 				break;
 			case 5:
-				strb[4].append(str+ " ");
+				strb[4].append(str+ ",");
 				break;
 			case 6:
-				strb[5].append(str+ " ");
+				strb[5].append(str+ ",");
 				break;
 			case 7:
-				strb[6].append(str+ " ");
+				strb[6].append(str+ ",");
 				break;
 			}
 		}
-		model.addAttribute("dow", DayOfWeek.class);
+		for(int i = 0; i < 7; i++) {
+			strb[i].delete(strb[i].length()-1, strb[i].length());
+		}
+		//文字列から曜日を除いた部分を変数に入れる
+		DayOfWeek dow = LocalDate.now().getDayOfWeek();
+		String todayGarbage = strb[dow.getValue()-1].toString();
+		todayGarbage = todayGarbage.replaceFirst(".*:", "");
+		
+		model.addAttribute("todayGarbage", todayGarbage);
 		model.addAttribute("strb",strb);
 		model.addAttribute("schedules", schedules);
 		model.addAttribute("today",LocalDate.now());
